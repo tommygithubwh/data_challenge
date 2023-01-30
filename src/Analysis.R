6e64f5a46e0005ca56b0d66065ee82f726038e97
tommygithubwh <tommyworkinghard@gmail.com>
2023-01-30 17:30:45 +0000

    ARIMA model and Chow test for structural break after Covid and NICE guidelines

--- src/Analysis.R
+++ src/Analysis.R
@@ -1,11 +1,14 @@
 
 #############################################################
-# Draft script for SARIMA forecast on anti-epileptic volume # 
+# Draft script for ARIMA forecast on anti-epileptic volume # 
 #############################################################
 
 install.packages("forecast")
+install.packages("sarima")
 
+library(ggplot2)
 library(forecast)
+library(sarima)
 
 # Reading in data ---------------------------------------------------------
 
@@ -15,10 +18,43 @@ Ep_Drugs_Total = subset(Ep_Drugs_Total, select = -c(Group.1,Year) ) # drops the 
 
 Ep_Drugs_Total_time <- Ep_Drugs_Total[, "x"] # selects the column (volume) to be forecast
 
-Sarima <- auto.arima(Ep_Drugs_Total_time)
+Arima <- auto.arima((Ep_Drugs_Total_time),0,1,1, 0,1,1,12)
 
-summary(Sarima)
+summary(Arima)
 
-predictions <- forecast(Sarima, h=12) # Predicts the next 12 time points
+predictions <- forecast(Arima, h=12) # Predicts the next 12 time points
 
 plot(predictions) # basic graph with CIs
+
+
+#############################################################
+# Draft script for Chow Test on anti-epileptic volume # 
+#############################################################
+
+install.packages("strucchange")
+
+library(strucchange)
+
+ggplot(Ep_Drugs_Total, aes(x = Date, y = x)) +
+  geom_line()
+
+# Chow test for covid period
+
+sctest(Ep_Drugs_Total$x ~ Ep_Drugs_Total$Date, type = "Chow", point = 30)
+# The Chow test attempts to determine if there is a structural break in the data at some point.
+# In this case, it is the volume of drugs before and after covid in the UK (point 30 or April 2020)
+# F = 2.9248, p-value = 0.06192.  We have insufficient evidence to reject the null of a structural break before and after covid
+
+
+# Chow test for NICE epilepsy guidelines April 2022
+sctest(Ep_Drugs_Total$x ~ Ep_Drugs_Total$Date, type = "Chow", point = 54)
+
+# F = 0.23881, p-value = 0.7884.   We have insufficient evidence to reject the null of a structural break before and after NICE
+# Guidelines but sample for the period after the guidelines is small
+
+
+
+
+
+
+
